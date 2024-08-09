58b4422f0acbb3bc039990e20498fba927bfd271
mikikyn <baizhigit.akaev@gmail.com>
2024-08-09 20:10:31 +0600

    'reg'

---
+++ lessons/lesson_2.py
@@ -0,0 +1,41 @@
+# ООП
+# принципы ООП:
+#
+
+class Book:
+    strr = 400
+
+    def __init__(self, title, author, color):
+        self.title = title
+        self.author = author
+        self.color = color
+
+    def info(self):
+        print(self.title, self.author, self.color, self.strr)
+
+    def __str__(self):
+        return (f'Title: {self.title}, Author: {self.author},\n'
+                f'Color: {self.color}, strr: {self.strr}\n')
+
+    def __len__(self):
+        return len(self.title + self.author + self.color)
+
+# объект\экземпляр класса
+город_воров=Book('город воров', 'Байжигит', 'зеленый')
+капитанская_дочка=Book('Капитанская дочь', 'Пушкин', 'серый')
+
+beka=Book('ЭТОМИР', 'beka', 'black')
+print(len(beka))
+
+ls=[1,1,1,1,1]
+
+
+
+
+
+
+
+# город_воров.info()
+# капитанская_дочка.info()
+
+
