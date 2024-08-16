1f1b786015153417d7bd7d8a37fde7db5d945adc
mikikyn <baizhigit.akaev@gmail.com>
2024-08-16 19:18:10 +0600

    'lesson_4'

--- lessons/lesson_4.py
+++ lessons/lesson_4.py
@@ -1,1 +1,26 @@
 # принципы ооп инкасуляция абстракция множ наследование
+
+class Bank:
+    def __init__(self, name, balance, pin):
+        self.name = name
+        self._balance = balance
+        self.__pin = pin
+
+    def details(self):
+        print(f'pin is {self.__pin}')
+
+    def __str__(self):
+        return f'name: {self.name}\nbalance: {self._balance}'
+
+    def sbalance(self, new):
+        self._balance += new
+        print(self._balance)
+
+
+
+beka = Bank('bekbolot', 0, '1111')
+Bank.details(beka)
+beka.__pin = 1222
+beka.details()
+print(beka.__pin)
+beka.details()
